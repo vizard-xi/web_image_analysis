2b31e8babe007ea6621df1aabb7e6e24eaf19fb0
Kelvin Prince <kelvin.prince.336@gmail.com>
2019-07-23 16:31:04 +0000

    Web Image Analysises

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template, url_for
+import requests
 
 app = Flask(__name__)
+    
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    image_url = "https://www.sbs.com.au/topics/sites/sbs.com.au.topics/files/197886641.jpg"
+    response = requests.post(f"https://faceplusplus-faceplusplus.p.rapidapi.com/facepp/v3/detect?image_url={image_url}",
+      headers={
+        "X-RapidAPI-Host": "faceplusplus-faceplusplus.p.rapidapi.com",
+        "X-RapidAPI-Key": "e16b0cb017msh3716024e485f05ap18269fjsn9b2db0896d8a",
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+        )
+    
+#    print(response.text)
+
+    r = response.json()
+
+#    print(dict(r))
+
+
+    return render_template('home.html', index = r)
+
 
 @app.route('/about')
 def about():
