48b1e22b4aeb1d72cdbe3d30e74f31d7743a4f52
Kelvin Prince <kelvin.prince.336@gmail.com>
2019-07-23 10:24:02 +0000

    Update

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
